e611d39574c89ddd53fe51bc1ae2500aa8921bd7
Chinedu codePanda Ikechi <Neddymond10@gmail.com>
2021-01-14 22:40:08 +0100

    changes to file server

--- src/api/app.js
+++ src/api/app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
 require("./db/mongoose");
+const path = require("path");
 const port = process.env.PORT || 3000;
 
 // Routers
 const reportScheduleRouter = require("./Routes/reportScheduleRouter");
+
+// built in middleware to serve static content just as images, css, html etc
+app.use(express.static(path.join(__dirname, 'dist')));
+ 
+// all get requests will point to angular's index.html in dist folder
+app.get('/*', async (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
 
 app.use(express.json());
 app.use(reportScheduleRouter);
