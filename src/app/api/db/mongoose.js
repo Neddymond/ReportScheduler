574b6398273efed726fb501f2c149fccab8d1bda
Chinedu codePanda Ikechi <Neddymond10@gmail.com>
2021-01-15 13:25:53 +0100

    file restructure

--- src/app/api/db/mongoose.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectionURI = process.env.MONGODB_URL;
-
-mongoose.connect(connectionURI, {
-  useNewUrlParser: true,
-  useCreateIndex: true
-});
--- src/app/src/app/schedules.ts
+++
@@ -1,6 +0,0 @@
-export interface Schedule {
-  date: string,
-  name: string,
-  email: string,
-  document: File
-}
