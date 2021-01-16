b12f7d1d2be2af7094d83c67b92547a4034bed56
Chinedu codePanda Ikechi <Neddymond10@gmail.com>
2021-01-16 18:47:28 +0100

    no message

--- api/app.js
+++ api/app.js
@@ -14,11 +14,11 @@ app.use(express.json());
 app.use(reportScheduleRouter);
 
 // built in middleware to serve static content just as images, css, html etc
-app.use(express.static(path.join(__dirname, '/dist/reportSchedules')));
+app.use(express.static(path.join(__dirname, '../dist/reportSchedules')));
  
 // all get requests will point to angular's index.html in dist folder
 app.get('/*', async (req, res) => {
-    res.sendFile(path.join(__dirname, "/dist/reportSchedules/index.html"));
+    res.sendFile(path.join(__dirname, "../dist/reportSchedules/index.html"));
 });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
